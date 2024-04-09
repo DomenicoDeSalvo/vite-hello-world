963c2b0236a8fccc1f4f08918db51e9e37ec04fa
DomenicoDeSalvo <domenicodesalvo_96@yahoo.it>
2024-04-09 18:36:17 +0200

    exercise

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+  export default{
+    data(){
+      return {
+        title: 'La mia prima App con Vite'
+      }
+    }
+  }
+
+</script>
+
+<template>
+
+  <div>
+    <h1 class="title">{{title}}</h1>
+  </div>
+
+</template>
+
+<style>
+  .title{
+    font-size: 50px;
+    margin: 24px 0;
+    text-align: center;
+  }
+</style>
