fa054f4b6f92686beac9463fee0279eee4f266c0
DomenicoDeSalvo <domenicodesalvo_96@yahoo.it>
2024-04-11 00:26:43 +0200

    added bonus

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script>
 import AppTitle from "./components/AppTitle.vue";
+import AppHero from "./components/AppHero.vue";
+
 
 export default{
   components: {
-    AppTitle
+    AppTitle,
+    AppHero
   }
 }
 
@@ -11,6 +14,7 @@ export default{
 
 <template>
   <AppTitle/>
+  <AppHero/>
 </template>
 
 <style>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,26 @@
+<script>
+export default{
+
+}
+</script>
+
+<template>
+    <div class="hero">
+        <h1>ciaone</h1>
+    </div>
+
+</template>
+
+<style>
+.hero{
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center ;
+    background-image: url(/public/ciaone.jpg);
+    background-repeat: no-repeat;
+    background-position: center;
+}
+
+
+</style>
